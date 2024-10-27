806833e2c58291fb85ea545fa0e5cfee501d3f37
yuriy.evseev <yuriy.evseev@glasno.tech>
2024-10-27 23:03:56 +0300

    workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  assetPrefix: process.env.GITHUB_ACTIONS ? '/ToDoList/' : '',
+  basePath: process.env.GITHUB_ACTIONS ? '/ToDoList' : '',
+  
+  images: {
+    unoptimized: true,
+  },
+  
+  trailingSlash: true,
+};
 
 export default nextConfig;
